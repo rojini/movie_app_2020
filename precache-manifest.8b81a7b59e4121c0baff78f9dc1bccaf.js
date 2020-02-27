d701441dd47ce7e6c3456084f3d1038b3e81ddbe
rojini <shin2101@gmail.com>
2020-02-27 18:50:45 +0900

    Updates

---
+++ precache-manifest.8b81a7b59e4121c0baff78f9dc1bccaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48615d32192d91728d85dcbc53bd593a",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "38cabe462182fbe26070",
+    "url": "/movie_app_2020/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "e3aca23ad6fcf7f07508",
+    "url": "/movie_app_2020/static/js/2.18b08166.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_2020/static/js/2.18b08166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38cabe462182fbe26070",
+    "url": "/movie_app_2020/static/js/main.c66a5426.chunk.js"
+  },
+  {
+    "revision": "21664dc3e2d58fbc7515",
+    "url": "/movie_app_2020/static/js/runtime-main.de86b078.js"
+  }
+]);
